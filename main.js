eb68a78f09b06e60eea416ced45c9b3ef24ac63a
juliana bentoss <julianabentostrabajo@gmail.com>
2023-11-21 23:32:38 -0300

    cree archivo j

---
+++ main.js
@@ -0,0 +1,43 @@
+// 1)
+
+
+// function suma(a,b){ 
+   // let par = 0;//
+    //par = par + a;//
+    //par = par + b;//
+
+    // vamos a imprimir la suma//
+    //console.log('par es: ', par);//
+
+    
+//}
+
+
+
+//suma(5,5)//
+
+// 2)
+
+//operadores comparativos //
+
+//console.log (9 < 10) // true
+
+
+
+// 4)
+
+for(let i = 0; i <= 10; i++){
+console.log(i)
+}
+
+
+for(let i = 0; i <= 10; 1++){
+    if (i === 5)
+continue;
+
+}
+
+
+
+
+
